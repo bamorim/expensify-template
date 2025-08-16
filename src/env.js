21fcab3994b65670db1acd675bca3ab6887834d8
Bernardo Amorim <contato@bamorim.com>
2025-08-16 16:41:18 +0100

    feat: tRPC & NextAuth & Nodemailer

--- src/env.js
+++ src/env.js
@@ -11,6 +11,9 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    MAIL_SERVER: z.string().url().default("smtp://127.0.0.1:1025"),
+    MAIL_FROM: z.string().email().default("no-reply@expensify.com"),
+    AUTH_SECRET: z.string().min(1).default("notsosecret"),
   },
 
   /**
@@ -29,6 +32,9 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    MAIL_SERVER: process.env.MAIL_SERVER,
+    MAIL_FROM: process.env.MAIL_FROM,
+    AUTH_SECRET: process.env.AUTH_SECRET,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
--- src/server/__mocks__/db.ts
+++
@@ -1,9 +0,0 @@
-import { type PrismaClient } from "@prisma/client";
-import { beforeEach } from "vitest";
-import { mockDeep, mockReset } from "vitest-mock-extended";
-
-beforeEach(() => {
-  mockReset(db);
-});
-
-export const db = mockDeep<PrismaClient>();
